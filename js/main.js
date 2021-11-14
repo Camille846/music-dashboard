ea07f8957903feeaa3b5dbf9ce05d8d0173e5e3b
Camille <camillegomes74@gmail.com>
2021-11-13 23:49:53 -0300

    Feat: menu icon Event when clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const $menu = document.querySelector('.menu');
+const $nav = document.querySelector('.nav');
+let menuOpen = false;
+$menu.addEventListener('click', function(){
+	if(!menuOpen){
+		$menu.classList.add('open');
+    $nav.style.display = 'flex';
+    document.getElementById('menu-burger').style.marginLeft = '200px';
+    document.getElementById('listening__content').style.marginLeft = '50px';
+		menuOpen = true;
+	}
+	else{
+		$menu.classList.remove('open');
+    $nav.style.display = 'none';
+    document.getElementById('menu-burger').style.marginLeft = '0px';
+    document.getElementById('listening__content').style.marginLeft = '0px';
+		menuOpen = false;
+	}
+});
+
 let swiper = new Swiper(".mySwiper", {
     loop:true,
     pagination: {
